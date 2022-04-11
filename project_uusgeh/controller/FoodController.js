6dc3e419d1585c92a448bba3962552bf4d13c066
namuun <nrgnamuunaa@gmail.com>
2022-04-11 22:53:42 +0800

    api

--- project_uusgeh/controller/FoodController.js
+++ project_uusgeh/controller/FoodController.js
@@ -2,6 +2,21 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 const Food = require('../models/Food');
 
+const get_food = (req, res, next) => {
+    const id = req.params.id
+    Food.findOne({_id: id}, function( err, data ){
+        if (err) 
+        res.json({
+            success: false,
+            data: err,
+        });
+        else
+        res.json({
+            success: true,
+            data: data,
+        })
+    })
+}
 const get_foods = (req, res, next) => {
     Food.find({}, function( err, data ){
         if (err) 
@@ -39,7 +54,7 @@ const create_food = (req, res, next) => {
     })}
 };
 
-const update = (req, res, next) => {
+const update_food = (req, res, next) => {
     const data = req.body;
     const id = req.params.id;
     Food.updateOne({_id: id}, data, function (err, data) {
@@ -61,9 +76,27 @@ const delete_food = (req, res, next) => {
         .catch(err => res.json({success: false, data: err}))
 };
 
+const search_food = (req, res, next) => {
+    const foodName = req.body
+    Food.findOne({foodName: foodName}, function( err, data ){
+        if (err) 
+        res.json({
+            success: false,
+            data: err,
+        });
+        else
+        res.json({
+            success: true,
+            data: data,
+        })
+    })
+};
+
 module.exports = {
+    get_food,
     get_foods,
     create_food,
-    update,
-    delete_food
+    delete_food,
+    update_food,
+    search_food,
 }
--- project_uusgeh/routes/api.js
+++ project_uusgeh/routes/api.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const FoodController = require('../controller/FoodController');
+const OrderController = require('../controller/OrderController');
 const FoodValidator = require('../middleware/create_update')
 
 
 // Requests
 
+router.get('/food/:id', FoodController.get_food);
 router.get('/foods', FoodController.get_foods);
-// router.get('/food', FoodController.get_food);
-// router.get('/user/:userId', FoodController.find_user);
+router.post('/new/food', FoodValidator.createfood(), FoodController.create_food);
+router.post('/update/food/:id', FoodController.update_food);
+router.post('/delete/food/:id', FoodController.delete_food)
+router.post('/search', FoodController.search_food)
+
+router.post('/new/order', OrderController.create_order)
+router.get('/get/user/order/:userId', OrderController.get_user_order_by_userId)
+router.get('/get/order/:id', OrderController.get_order_by_id)
+router.post('/orders', OrderController.get_orders)
+router.post('/update/order', OrderController.update_order)
+router.post('/delete/order', OrderController.delete_order)
 
 
-router.post('/createfood', FoodValidator.createfood(), FoodController.create_food);
-
-router.post('/updateFood/:id', FoodController.update);
-
-router.post('/deleteFood/:id', FoodController.delete_food)
-// router.post('/createFood',FoodValidator.createfood(), FoodController.create_food)
-// router.post('/addUser', FoodController.add_user);
-// router.post('/deleteOrder/:orderId', FoodController.delete_order)
-// router.post('/addOrder', FoodController.add_order);
-// router.post('/deleteUser/:userId', FoodController.delete_user)
 
 module.exports = router;
