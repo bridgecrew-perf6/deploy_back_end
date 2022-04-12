cc6a4b9e38e09a39a2cd0c044630bc011266cfe8
Mstars_lab1_04 <mstars_lab1_04@Mstars-lab1-04s-iMac.local>
2022-04-12 13:37:07 +0800

    req put del

--- project_uusgeh/routes/api.js
+++ project_uusgeh/routes/api.js
@@ -11,15 +11,15 @@ const FoodValidator = require('../middleware/create_update')
 router.get('/food/:id', FoodController.get_food);
 router.get('/foods', FoodController.get_foods);
 router.post('/new/food', FoodController.create_food);
-router.post('/update/food/:id', FoodController.update_food);
-router.post('/delete/food/:id', FoodController.delete_food)
+router.put('/update/food/:id', FoodController.update_food);
+router.delete('/delete/food/:id', FoodController.delete_food)
 router.post('/search', FoodController.search_food)
 
 router.post('/new/order', OrderController.create_order)
 router.get('/get/user/order/:userId', OrderController.get_user_order_by_userId)
 // router.get('/get/order/:id', OrderController.get_order_by_id)
 router.post('/orders', OrderController.get_orders)
-router.post('/update/order', OrderController.update_order)
-router.post('/delete/order', OrderController.delete_order)
+router.put('/update/order', OrderController.update_order)
+router.delete('/delete/order', OrderController.delete_order)
 
 module.exports = router;
